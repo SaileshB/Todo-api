3b088df1b2e2833c1c90f11cd34378a377484e62
Sailesh Bajracharya <nhuja08@gmail.com>
2016-07-27 16:35:58 +0545

    add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,29 @@
+// var person = {
+// 	name: 'Sailesh',
+// 	age: 21
+// };
+
+// function updatePerson(obj) {
+// 	// obj = {
+// 	// 	name:'Sailesh',
+// 	// 	age: 23 
+// 	// };
+// 	obj.age = 23;
+
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+//Array eg.
+
+var grades = [15,20];
+
+function addGrade(grade) {	
+	
+	grade.push(39);
+	debugger;
+}
+
+addGrade(grades);
+console.log(grades);
