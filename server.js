5002ff5541595475fec6574bc5f96ea11b625fe4
Sailesh Bajracharya <nhuja08@gmail.com>
2016-07-28 16:38:47 +0545

    add sequelize demo

--- server.js
+++ server.js
@@ -6,30 +6,34 @@ var _ = require('underscore');
 var app = express();
 
 
-var PORT = process.env.PORT ||3000;
+var PORT = process.env.PORT || 3000;
 var todos = [];
 var todoNextId = 1;
 
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
 	res.send('TODO api root');
 });
 
 //GET /todos?completed=true
-app.get('/todos', function(req, res){
+app.get('/todos', function(req, res) {
 	var queryParams = req.query;
 
 	var filteredTodos = todos;
 
 	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-		filteredTodos = _.where(filteredTodos, {completed:true});
+		filteredTodos = _.where(filteredTodos, {
+			completed: true
+		});
 	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-		filteredTodos = _.where(filteredTodos, {completed:false});
+		filteredTodos = _.where(filteredTodos, {
+			completed: false
+		});
 	}
 
 	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-		filteredTodos = _.filter(filteredTodos, function (todo) {
+		filteredTodos = _.filter(filteredTodos, function(todo) {
 			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
 		});
 	}
@@ -37,10 +41,12 @@ app.get('/todos', function(req, res){
 	res.json(filteredTodos);
 });
 //GET /todos/:id
-app.get('/todos/:id',function (req, res) {
+app.get('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos,{id: todoId});
-	 /* same code using underscore!!!
+	var matchedTodo = _.findWhere(todos, {
+		id: todoId
+	});
+	/* same code using underscore!!!
 	 // var matchedTodo;
 
 	// todos.forEach(function (todo) {
@@ -52,49 +58,55 @@ app.get('/todos/:id',function (req, res) {
 
 	if (matchedTodo) {
 		res.json(matchedTodo);
-	} else{
+	} else {
 		res.status(404).send();
-	}	
+	}
 });
 
 //POST /todos/
-app.post('/todos', function (req, res) {
+app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
-	 
 
 
-	if (!_.isBoolean(body.completed)|| !_.isString(body.description) || body.description.trim().length === 0) {
+
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
 		return res.status(400).send();
 	}
-	
+
 	body.description = body.description.trim();
 	//add id field
 	body.id = todoNextId++;
-	
+
 	//push data ont the array
 	todos.push(body);
-	
+
 	res.json(body);
 });
 
 //delete /todos/:id
- 
-app.delete('/todos/:id',function (req, res) {
+
+app.delete('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos,{id: todoId});
+	var matchedTodo = _.findWhere(todos, {
+		id: todoId
+	});
 	if (!matchedTodo) {
-		res.status(404).json({"error":"no todo found with that ID"});
+		res.status(404).json({
+			"error": "no todo found with that ID"
+		});
 	} else {
-		todos = _.without(todos,matchedTodo);
+		todos = _.without(todos, matchedTodo);
 		res.json(matchedTodo);
 	}
 });
 
 //PUT /todos/:id
-app.put('/todos/:id', function(req,res){
+app.put('/todos/:id', function(req, res) {
 
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos,{id: todoId});
+	var matchedTodo = _.findWhere(todos, {
+		id: todoId
+	});
 	var body = _.pick(req.body, 'description', 'completed');
 	var validAttributes = {};
 
@@ -107,7 +119,7 @@ app.put('/todos/:id', function(req,res){
 		validAttributes.completed = body.completed;
 	} else if (body.hasOwnProperty('completed')) {
 		return res.status(400).send();
-	} 
+	}
 
 	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
 		validAttributes.description = body.description;
@@ -121,9 +133,9 @@ app.put('/todos/:id', function(req,res){
 
 
 
-}); 
+});
 
 
-app.listen(PORT, function(){
-	console.log('express listening on port '+PORT+ '!');
+app.listen(PORT, function() {
+	console.log('express listening on port ' + PORT + '!');
 })
